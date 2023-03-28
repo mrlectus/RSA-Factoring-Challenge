313d402e76a343354a8a6d198cc5b5a29ad10154
MrLrctus <lilbrown3000@gmail.com>
2023-03-28 22:31:50 +0100

    feat: Big integer added

--- factor/src/main.rs
+++ factor/src/main.rs
@@ -1,13 +1,18 @@
-use std::{env, fs::read_to_string};
-pub fn find_factors(num: i128) -> (i128, i128) {
-    let end = num as f64;
-    let end = end.sqrt();
-    for i in 2..end.floor() as i128 + 1 {
-        if num % i == 0 {
-            return ((num / i), i);
+use std::{env, fs::read_to_string, str::FromStr};
+use num_bigint::{BigUint, ToBigUint};
+
+
+pub fn find_factors(value: &str) -> (BigUint, BigUint) {
+    let mut left = 2.to_biguint().unwrap();
+    let right = &BigUint::from_str(&value).unwrap();
+    let right_end = &BigUint::from_str(&value).unwrap().sqrt();
+    while left <= *right_end {
+        if right % &left == BigUint::new(vec![0]) {
+            return (right / &left, left);
         }
+        left = left + 1.to_biguint().unwrap();
     }
-    (-1, -1)
+    (BigUint::new(vec![0]), BigUint::new(vec![0]))
 }
 
 fn main() {
@@ -15,20 +20,19 @@ fn main() {
     let file_path = &args[1];
     let f = read_to_string(file_path).expect("cannot find file");
     for x in f.lines() {
-        let (v1, v2) = find_factors(x.parse().unwrap());
+        let (v1, v2) = find_factors(&x);
         println!("{x}={v1}*{v2}");
     }
 }
 
 #[cfg(test)]
 mod test {
+    use num_bigint::ToBigUint;
+
     use crate::find_factors;
 
     #[test]
     fn test_find_factors() {
-        assert_eq!(
-            find_factors(1718944270642558716715),
-            (343788854128511743343, 5)
-        );
+        assert_eq!(find_factors("9"), (3.to_biguint().unwrap(), 3.to_biguint().unwrap()));
     }
 }
