53b6583ec82520227721ea39c0f54115ec4a3fdb
BrownCarter <lilbrown3000@gmail.com>
2023-03-29 18:51:01 +0100

    hope this works

--- factor/src/main.rs
+++ factor/src/main.rs
@@ -1,18 +1,21 @@
 use std::{env, fs::read_to_string, str::FromStr};
-use num_bigint::{BigUint, ToBigUint};
 
+use rug::Integer;
 
-pub fn find_factors(value: &str) -> (BigUint, BigUint) {
-    let mut left = 2.to_biguint().unwrap();
-    let right = &BigUint::from_str(&value).unwrap();
-    let right_end = &BigUint::from_str(&value).unwrap().sqrt();
-    while left <= *right_end {
-        if right % &left == BigUint::new(vec![0]) {
-            return (right / &left, left);
+pub fn find_factors(value: &str) -> (Integer, Integer) {
+    let int = Integer::from_str(value).unwrap();
+    if int.is_even() {
+        return (int / 2, Integer::from(2));
+    } else {
+        let mut i = Integer::from(2);
+        while i <= int {
+            if &int % Integer::from(&i) == 0 {
+                return (int / Integer::from(&i), Integer::from(&i));
+            }
+            i += Integer::from(1);
         }
-        left = left + 1.to_biguint().unwrap();
     }
-    (BigUint::new(vec![0]), BigUint::new(vec![0]))
+    (Integer::from(2), Integer::from(0))
 }
 
 fn main() {
@@ -20,19 +23,18 @@ fn main() {
     let file_path = &args[1];
     let f = read_to_string(file_path).expect("cannot find file");
     for x in f.lines() {
-        let (v1, v2) = find_factors(&x);
+        let (v1, v2) = find_factors(x);
         println!("{x}={v1}*{v2}");
     }
 }
 
 #[cfg(test)]
 mod test {
-    use num_bigint::ToBigUint;
-
     use crate::find_factors;
-
+    use rug::Integer;
     #[test]
     fn test_find_factors() {
-        assert_eq!(find_factors("9"), (3.to_biguint().unwrap(), 3.to_biguint().unwrap()));
+        assert_eq!(find_factors("4"), (Integer::from(2), Integer::from(2)));
+        assert_eq!(find_factors("9"), (Integer::from(3), Integer::from(3)));
     }
 }
