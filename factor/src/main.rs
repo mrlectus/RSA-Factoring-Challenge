953efe90ae868d4700c3419f5cccb343c5220530
BrownCarter <lilbrown3000@gmail.com>
2023-03-30 13:11:51 +0100

    fix

--- factor/src/main.rs
+++ factor/src/main.rs
@@ -1,23 +1,23 @@
-use std::{env, fs::read_to_string, str::FromStr};
+use std::{env, fs::read_to_string};
 
-use rug::Integer;
-
-pub fn find_factors(value: &str) -> (Integer, Integer) {
-    let int = Integer::from_str(value).unwrap();
-    if int.is_even() {
-        return (int / 2, Integer::from(2));
-    } else {
-        let mut i = Integer::from(3);
-        while i <= int {
-            if &int % Integer::from(&i) == 0 {
-                return (int / Integer::from(&i), Integer::from(&i));
-            }
-            i += Integer::from(2);
-        }
-    }
-    (Integer::from(0), Integer::from(0))
-}
-
+// use rug::Integer;
+//
+// pub fn find_factors(value: &str) -> (Integer, Integer) {
+//     let int = Integer::from_str(value).unwrap();
+//     if int.is_even() {
+//         return (int / 2, Integer::from(2));
+//     } else {
+//         let mut i = Integer::from(3);
+//         while i <= int {
+//             if &int % Integer::from(&i) == 0 {
+//                 return (int / Integer::from(&i), Integer::from(&i));
+//             }
+//             i += Integer::from(2);
+//         }
+//     }
+//     (Integer::from(0), Integer::from(0))
+// }
+//
 pub fn find_factors_v2(value: u128) -> (u128, u128) {
     if value % 2 == 0 {
         return (value / 2, 2);
@@ -45,11 +45,11 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use crate::find_factors;
-    use rug::Integer;
+    // use crate::find_factors;
+    // use rug::Integer;
     #[test]
     fn test_find_factors() {
-        assert_eq!(find_factors("4"), (Integer::from(2), Integer::from(2)));
-        assert_eq!(find_factors("9"), (Integer::from(3), Integer::from(3)));
+        // assert_eq!(find_factors("4"), (Integer::from(2), Integer::from(2)));
+        // assert_eq!(find_factors("9"), (Integer::from(3), Integer::from(3)));
     }
 }
